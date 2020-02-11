8ea7a128680eeb2fabf8dde7a1adc577227d9412
iggy-pigy <ilgakokorisa@gmail.com>
2020-02-11 19:57:21 +0000

    Created QuizData.js file

---
+++ src/QuizData.js
@@ -0,0 +1,67 @@
+export const QuizData = [
+  {
+    id: 1,
+    question: "What is your favorite drink?",
+    options: [
+      "sangria",
+      "red wine",
+      "Coca cola",
+      "mango lassi",
+      "gin",
+      "caipirinha"
+    ],
+    answer: ""
+  },
+  {
+    id: 2,
+    question: "What is your favorite movie?",
+    options: [
+      "Vicky Cristina Barcelona",
+      "Amelie",
+      "Disney films",
+      "Bollywood",
+      "James Bond movies",
+      "Cidade de Deus"
+    ],
+    answer: ""
+  },
+  {
+    id: 3,
+    question: "What is your favorite activity?",
+    options: [
+      "sunbathing",
+      "falling in love",
+      "Disneyland",
+      "yoga",
+      "drinking tea with the Queen",
+      "surfing"
+    ],
+    answer: ""
+  },
+  {
+    id: 4,
+    question: "Which monument would you like to see?",
+    options: [
+      "La Sagrada Familia",
+      "Tour Eiffel",
+      "Disneyland",
+      "Taj Mahal",
+      "Big Ben",
+      "Christ the Redeemer"
+    ],
+    answer: ""
+  },
+  {
+    id: 5,
+    question: "Who is your favorite artist?",
+    options: [
+      "Salvador Dali",
+      "Claude Monet",
+      "Walt Disney",
+      "Priyanka Chopra",
+      "David Hockney",
+      "Street Art"
+    ],
+    answer: ""
+  }
+];
