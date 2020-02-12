223aa99b9489fd1d2817c68388b4d37ef36683d4
iggy-pigy <ilgakokorisa@gmail.com>
2020-02-12 18:51:41 +0000

    Added componentDidMount and shuffleArray function

--- src/App.js
+++ src/App.js
@@ -3,7 +3,57 @@ import QuizData from "./components/QuizData";
 import Quiz from "./components/Quiz";
 import "./App.css";
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+      questionId: 1,
+      question: '',
+      answerOptions: [],
+      answer: '',
+      answersCount: {},
+      result: ''
+    };
+
+    this.handleAnswerSelected = this.handleAnswerSelected.bind(this);
+  }
+
+  componentDidMount() {
+    // shuffledAnswerOptions will display answers in random order
+    const shuffledAnswerOptions = quizQuestions.map(question =>
+     this.shuffleArray(question.answers)
+    );
+    this.setState({
+      question: quizQuestions[0].question,
+      answerOptions: shuffledAnswerOptions[0]
+    });
+  }
+
+  // shuffledArray function
+  shuffleArray(array) {
+    var currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+
+
+
   return (
     <div className="App">
       <header className="App-header"></header>
