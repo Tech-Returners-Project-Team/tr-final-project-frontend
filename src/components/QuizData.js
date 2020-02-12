1c96897fec9e7b28945e276fb9cec81e198761df
iggy-pigy <ilgakokorisa@gmail.com>
2020-02-12 16:32:27 +0000

    Created QuizData.js file

---
+++ src/components/QuizData.js
@@ -0,0 +1,33 @@
+export const QuizData = [
+    {
+      question: "What is your favorite drink?",
+      answers: [
+        { city: "Barcelona", option: "sangria" },
+        { city: "Paris", option: "red wine" },
+        { city: "Orlando", option: "Coca cola" },
+        { city: "Agra", option: "mango lassi" },
+        { city: "London", option: "gin" },
+        { city: "Rio de Janeiro", option: "caipirinha" }
+      ]
+    },
+    {
+      question: "What is your favorite movie?",
+      answers: [
+        { city: "Barcelona", option: "Vicky Cristina Barcelona" },
+        { city: "Paris", option: "Amelie" },
+        { city: "Orlando", option: "Disney films" },
+        { city: "Agra", option: "Bollywood" },
+        { city: "London", option: "James Bond movies" },
+        { city: "Rio de Janeiro", option: "Cidade de Deus" }
+      ]
+    },
+    {
+      question: "What is your favorite activity?",
+      answers: [
+        { city: "Barcelona", option: "sunbathing" },
+        { city: "Paris", option: "falling in love" },
+        { city: "Orlando", option: "Disneyland" },
+        { city: "Agra", option: "yoga" },
+        { city: "London", option: "drinking tea with the Queen" },
+        { city: "Rio de Janeiro", option: "surfing" }
+      ]
