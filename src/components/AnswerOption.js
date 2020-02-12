e6609d137b36dd7e3acad64c85b2716d1fa28d30
iggy-pigy <ilgakokorisa@gmail.com>
2020-02-12 17:00:02 +0000

    Created AnswerOption.js file

---
+++ src/components/AnswerOption.js
@@ -0,0 +1,21 @@
+import React from "react";
+function AnswerOption(props) {
+  return (
+    <li className="answerOption">
+      <input
+        type="radio"
+        className="radioCustomButton"
+        name="radioGroup"
+        checked={props.answerCity === props.answer}
+        id={props.answerCity}
+        value={props.answerCity}
+        disabled={props.answer}
+        onChange={props.onAnswerSelected}
+      />
+      <label className="radioCustomLabel" htmlFor={props.answerCity}>
+        {props.answerOption}
+      </label>
+    </li>
+  );
+}
+export default AnswerOption;
