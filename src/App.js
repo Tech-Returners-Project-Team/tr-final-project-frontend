3c64059700bc60d0bb397c16abec642c216dd0e2
iggy-pigy <ilgakokorisa@gmail.com>
2020-02-12 19:26:59 +0000

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import QuizData from "./components/QuizData";
 import Quiz from "./components/Quiz";
 import "./App.css";
 
-class App extends Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
 
