a4c0053deb7750ce3bf8ba6ed71a1f932b44f4c6
iggy-pigy <ilgakokorisa@gmail.com>
2020-02-26 19:16:31 +0000

    Updates

---
+++ precache-manifest.c39b43a2779f21428f1264ddbcbbdd55.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2261eca8509534b7a1c84b6458e3dda",
+    "url": "/Tech-Returners-Project-Team/tr-final-project-frontend/index.html"
+  },
+  {
+    "revision": "38e8685edb3566537d56",
+    "url": "/Tech-Returners-Project-Team/tr-final-project-frontend/static/css/main.199981d4.chunk.css"
+  },
+  {
+    "revision": "061d77f0d767a5038c83",
+    "url": "/Tech-Returners-Project-Team/tr-final-project-frontend/static/js/2.3f4938c1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Tech-Returners-Project-Team/tr-final-project-frontend/static/js/2.3f4938c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38e8685edb3566537d56",
+    "url": "/Tech-Returners-Project-Team/tr-final-project-frontend/static/js/main.1ea07faa.chunk.js"
+  },
+  {
+    "revision": "b8f6eb4483ebd07ddb37",
+    "url": "/Tech-Returners-Project-Team/tr-final-project-frontend/static/js/runtime-main.37fa6474.js"
+  },
+  {
+    "revision": "ad9f26394ee8bb89db7091f92f49831a",
+    "url": "/Tech-Returners-Project-Team/tr-final-project-frontend/static/media/three.ad9f2639.gif"
+  },
+  {
+    "revision": "a0b84493f40fe6c4669505d4f45a19ff",
+    "url": "/Tech-Returners-Project-Team/tr-final-project-frontend/static/media/travel.a0b84493.gif"
+  }
+]);
